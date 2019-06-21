0d4eaed6beac6c8404653ed9908652b3297909f9
nick <njrusmc@Gmail.com>
2019-06-20 21:09:10 -0400

    curl, postman, some organizing

---
+++ m5/get_devices.sh
@@ -0,0 +1,4 @@
+curl -X GET \
+  -H 'Content-Type: application/json' \
+  -H 'X-Auth-Token: eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiI1YmQ5OGQzNWIyYmVhMDAwNGMzZWM5YmYiLCJhdXRoU291cmNlIjoiaW50ZXJuYWwiLCJ0ZW5hbnROYW1lIjoiVE5UMCIsInJvbGVzIjpbIjViZDM2MzRiYjJiZWEwMDA0YzNlYmI1YSJdLCJ0ZW5hbnRJZCI6IjViZDM2MzRhYjJiZWEwMDA0YzNlYmI1OCIsImV4cCI6MTU2MTA4MjI5NywidXNlcm5hbWUiOiJkZXZuZXR1c2VyIn0.o_xIhxENsiZpoL4WL0BvZLcb6Gy-Gxet21fWyiG1trs' \
+  https://sandboxdnac.cisco.com/dna/intent/api/v1/network-device/
